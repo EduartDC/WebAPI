e1137dcab968a11fa9d9488d82ddce298c205aa2
EDUART USSIEL DIRCIO CAYON <zS19030173@estudiantes.uv.mx>
2023-11-12 17:43:23 -0600

    Add project files.

---
+++ WebAPI/Models/Alumno.cs
@@ -0,0 +1,11 @@
+﻿namespace WebAPI.Models
+{
+    public class Alumno
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public bool EstaAprobado { get; set; }
+        public string Secreto { get; set; }
+
+    }
+}
